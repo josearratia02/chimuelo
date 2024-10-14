a769868617ea76a901ada933343f4707acb27343
Chimuelo <josearratia615@gmail.com>
2024-10-14 19:41:45 -0300

    adding spring files

---
+++ src/main/java/com/chimu/chimuelo/ChimueloApplication.java
@@ -0,0 +1,13 @@
+package com.chimu.chimuelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChimueloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChimueloApplication.class, args);
+	}
+
+}
